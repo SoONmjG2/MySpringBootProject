6b642eeee2f47b19ae2fe8aa7262d84c683368f1
손민정 <ecolead@naver.com>
2025-08-29 13:27:31 +0900

    .

--- src/main/java/com/rookies4/myspringboot/entity/Student.java
+++ src/main/java/com/rookies4/myspringboot/entity/Student.java
@@ -20,11 +20,11 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "student_id")
     private Long id;
-    
+
     //이름
     @Column(nullable = false)
     private String name;
-    
+
     //학번
     @Column(unique = true, nullable = false)
     private String studentNumber;
@@ -35,7 +35,13 @@ public class Student {
      */
     //1:1관계 지연로딩
     @OneToOne(fetch = FetchType.LAZY,
-              mappedBy = "student",
-              cascade = CascadeType.ALL)
+            mappedBy = "student",
+            cascade = CascadeType.ALL)
     private StudentDetail studentDetail;
+
+    //N:1 Student:Department 관계에서 N쪽에 해당하는 Student가 Owner
+    //department변수는 테이블의 FK와 매핑되는 필드임.
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
 }
